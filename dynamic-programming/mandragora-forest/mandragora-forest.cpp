0ae9894be658b662261dca43550e66f71bfb62af
Agusti Tomas <agusti.tomas@gmail.com>
2016-08-20 17:21:15 +0200

    pass test case 1

--- dynamic-programming/mandragora-forest/mandragora-forest.cpp
+++ dynamic-programming/mandragora-forest/mandragora-forest.cpp
@@ -17,34 +17,41 @@ void printVector(const vector<unsigned long int>& v, const string& name) {
     cout << endl;
 }
 
-unsigned long int findMaxExperienceDP(const vector<unsigned long int>& h, unsigned int i, unsigned int s, unsigned int p, 
-		vector<unsigned long int>& withS, vector<unsigned long int>& withP, vector<bool>& calculated) {
-	if (i == h.size()-1) {
-		withS[i] = (p + s * h[i]);
-		withP[i] = (p + s * h[i]);
-		calculated[i] = true;
-	} else if (i == 0) {
-		withS[i] = findMaxExperienceDP(h, i+1, s+1, p, withS, withP, calculated);
-		withP[i] = findMaxExperienceDP(h, i+1, s, p + s * h[i], withS, withP, calculated);
-	} else if (!calculated[i]) {
-		if (debug) {
-			printVector(withS, "withS iteration (before recursion) : " + to_string(i));
-			printVector(withP, "withP iteration (before recursion) : " + to_string(i));
+unsigned long long int findMaxExperienceIt(const vector<unsigned long int>& h) {
+	unsigned long long int max = 0;
+	for (auto i = 0; i < h.size(); ++i) {
+		unsigned long long int sum = 0;
+		for (auto j = 0; j <= i; ++j) {
+			sum += h[j] * (h.size() - i);
 		}
-		withS[i] = findMaxExperienceDP(h, i+1, s+1, p, withS, withP, calculated);
-		withP[i] = findMaxExperienceDP(h, i+1, s, p + s * h[i], withS, withP, calculated);
-		calculated[i] = true;
-		if (debug) {
-			printVector(withS, "withS iteration : " + to_string(i));
-			printVector(withP, "withP iteration : " + to_string(i));
-		}
-	} else if (calculated[i]) {
-		if (debug) {
-			cout << "Already calculated : " << i << endl;
+		// cout << "sum is : " << sum << " and max is : " << max << " iteration is : " << i << endl;
+		if (sum > max) {
+			max = sum;
 		}
 	}
-	return max(withS[i], withP[i]);
+	return max;
 }
+
+// unsigned long int findMaxExperienceDP(const vector<unsigned long int>& h, unsigned int i, unsigned int s, unsigned int p,
+// 		vector<unsigned long int>& withS, vector<unsigned long int>& withP, vector<bool>& calculated) {
+//
+//
+//
+// 		if (debug) {
+// 			printVector(withS, "withS iteration (before recursion) : " + to_string(i));
+// 			printVector(withP, "withP iteration (before recursion) : " + to_string(i));
+// 		}
+//
+// 		if (debug) {
+// 			printVector(withS, "withS iteration : " + to_string(i));
+// 			printVector(withP, "withP iteration : " + to_string(i));
+// 		}
+// 	} else if (calculated[i]) {
+// 		if (debug) {
+// 			cout << "Already calculated : " << i << endl;
+// 		}
+// 	}
+// }
 
 // unsigned long int findMaxExperience(const vector<unsigned long int>& h, unsigned int i, unsigned int s, unsigned int p,
 // 		vector<unsigned long int>& withS, vector<unsigned long int>& withP, vector<bool>& calculated, vector<unsigned long int>& maxExp) {
@@ -93,7 +100,7 @@ int main() {
 // 		}
 		
 		sort(healthPoints.begin(), healthPoints.end());
-		// reverse(healthPoints.begin(), healthPoints.end());
+		reverse(healthPoints.begin(), healthPoints.end());
 		
 		if (debug) {
 			printVector(healthPoints, "healthPoints for case " + to_string(i) + " sorted");
@@ -102,7 +109,8 @@ int main() {
 		vector<unsigned long int> withS(healthPoints.size(), 0);
 		vector<unsigned long int> withP(healthPoints.size(), 0);
 		vector<bool> calculated(healthPoints.size(), false);
-		auto maxExperience = findMaxExperienceDP(healthPoints, 0, 1, 0, withS, withP, calculated);
+		auto maxExperience = findMaxExperienceIt(healthPoints);
+		// auto maxExperience = findMaxExperienceDP(healthPoints, 0, 1, 0, withS, withP, calculated);
 		cout << maxExperience << endl;
     }
     
