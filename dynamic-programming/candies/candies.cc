ccddb361edf82ba280055f533163518f00ff91fc
Agusti Tomas <agusti.tomas@gmail.com>
2016-08-22 16:11:56 +0200

    more tests passing but something fishy with the equals case

--- dynamic-programming/candies/candies.cc
+++ dynamic-programming/candies/candies.cc
@@ -6,11 +6,11 @@
 
 using namespace std;
 
-constexpr bool debug = false;
+constexpr bool debug = true;
 
 template <typename Container>
 void printContainer(const Container& c, const string& name) {
-    cout << "Printing vector - " << name << " of size = " << c.size() << " : [";
+    cout << "Printing container - " << name << " of size = " << c.size() << " : [";
     for (auto i = 0; i < c.size()-1; ++i) {
         cout << c[i] << ",";
     }
@@ -21,30 +21,61 @@ unsigned long int findMinCandies(const vector<unsigned long int>& r) {
 	auto minCandies = static_cast<unsigned long int>(1);
 	auto candyDiffUp = static_cast<unsigned long int>(1);
 	auto candyDiffDown = static_cast<unsigned long int>(0);
+    auto lastUp = static_cast<unsigned long int>(0);
+    vector<unsigned int> lastUps;
+    
 	
 	for (auto i = 1; i < r.size(); ++i) {
-		if (r[i] <= r[i-1]) {
+		if (r[i] < r[i-1]) {
 			if (debug) {
-				cout << "r[i] <= r[i-1]";
+				cout << "r[i] <  r[i-1]";
 			}
-			minCandies += 1 + candyDiffDown;
+            if (candyDiffDown == 0) {
+                lastUps.push_back(i);
+                lastUp = candyDiffUp;
+            }
 			candyDiffUp = 1;
 			++candyDiffDown;
-		} else {
+			minCandies += candyDiffDown;
+		} else if(r[i] == r[i-1]) {
 			if (debug) {
-				cout << "r[i] > r[i-1]";
+				cout << "r[i] <  r[i-1]";
 			}
-			minCandies += 1 + candyDiffUp;
+			candyDiffUp = 1;
+			minCandies += candyDiffUp;
+        } else {
+			if (debug) {
+				cout << "r[i] >  r[i-1]";
+			}
+            if (lastUp > 0 && lastUp <= candyDiffDown) {
+                if (debug) {
+                    cout << " (adding lastUp = " << candyDiffDown - lastUp + 1 << ")";
+                }
+                minCandies += candyDiffDown - lastUp + 1;
+                lastUp = 0;
+            }
 			candyDiffDown = 0;
 			++candyDiffUp;
+            minCandies += candyDiffUp;
 		}
 		if (debug) {
 			cout << " minCandies = " << minCandies << " - candyDiffUp = " << candyDiffUp
-				 << " - candyDiffDown = " << candyDiffDown << " r[" << i << "] = " 
-				 << r[i] << " : r[" << i-1 << "] = " << r[i-1] << endl;
+				 << " - candyDiffDown = " << candyDiffDown << " - lastUp = " << lastUp
+                 << " | r[" << i << "] = " << r[i] << " : r[" << i-1 
+                 << "] = " << r[i-1] << endl;
 		}
 	}
-	
+    
+    if (lastUp > 0 && lastUp <= candyDiffDown) {
+        cout << "(adding LAST lastUp = " << candyDiffDown - lastUp + 1 << ")" << endl;
+        minCandies += candyDiffDown - lastUp + 1;
+        lastUp = 0;
+    }
+    
+    if (debug) {
+        printContainer(lastUps, "lastUps");
+    }
+	    
 	return minCandies;
 }
 
---
+++ dynamic-programming/nikita-and-the-game/nikita-and-the-game.cc
@@ -0,0 +1,20 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    auto cases = -1;
+    cin >> cases;
+    
+    for (auto i = 0; i < cases; i++) {
+        auto size = -1;
+        cin >> size;
+    }
+    
+    return 0;
+}
