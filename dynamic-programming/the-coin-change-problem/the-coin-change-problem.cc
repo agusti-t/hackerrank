97972d258c1732e83b0674529d927f53358a93ef
Agusti Tomas <agusti.tomas@gmail.com>
2016-09-06 11:15:18 +0200

    coin change initial commit

---
+++ dynamic-programming/the-coin-change-problem/the-coin-change-problem.cc
@@ -0,0 +1,42 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+constexpr bool debug = true;
+
+using UInt = unsigned int;
+
+template <typename Container>
+void printContainer(const Container& c, const string& name) {
+    cout << "Printing container - " << name << " of size = " << c.size() << " : [";
+    for (auto i = 0; i < c.size()-1; ++i) {
+        cout << c[i] << ",";
+    }
+    cout << c[c.size()-1] << "]" << endl;
+}
+
+int main() {
+    auto change = static_cast<UInt>(0);
+    cin >> change;
+	
+	auto nCoins = static_cast<UInt>(0);
+	cin >> nCoins;
+    
+	vector<UInt> coins;
+    for (auto i = 0; i < nCoins; ++i) {
+        auto coin = static_cast<UInt>(0);
+ 	   	cin >> coin;
+        coins.push_back(coin);
+	}
+	
+	if (debug) printContainer(coins, "coins");
+	
+	// auto nChange = findChange(change, coins);
+	// cout << nChange << endl;
+	
+	return 0;
+}
